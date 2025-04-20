39383dbdad622137c745b067e215405b036495a6
anwartarekA <anwartarek719@gmail.com>
2025-04-20 16:16:46 +0100

    react step component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+const steps = ["Learn React ⚛", "Apply Technologies 💢", "Make Practices 🏆"];
+export default function App() {
+  const [step, setStep] = useState(0);
+  const handlePrevious = () => {
+    if (step >= 1) setStep(step - 1);
+  };
+  const handleNext = () => {
+    if (step < 2) setStep(step + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <Numbers step={step} />
+      <Msgs step={step} />
+      <Btns previous={handlePrevious} next={handleNext} />
     </div>
   );
 }
 
-export default App;
+function Numbers({ step }) {
+  return (
+    <div className="numbers">
+      <div className={`one ${step >= 0 ? "active" : ""}`}>1</div>
+      <div className={`two ${step >= 1 ? "active" : ""}`}>2</div>
+      <div className={`three ${step === 2 ? "active" : ""}`}>3</div>
+    </div>
+  );
+}
+function Msgs({ step }) {
+  return (
+    <p>
+      {`Step ${step + 1}: `}
+      {steps[step]}
+    </p>
+  );
+}
+
+function Btns({ previous, next }) {
+  return (
+    <div className="btns">
+      <button onClick={previous}>previous</button>
+      <button onClick={next}>next</button>
+    </div>
+  );
+}
